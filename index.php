bbf83b58de292b63da315d0a8f43c99c4ed60701
mariliaferreira <mariliaferreiraa@gmail.com>
2015-05-15 11:39:38 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<title>SwB Experience - 1st PUCPR SWB Egress Meeting</title>
+		<meta name="description" content="1º Meet Up Science Without Borders Alumni" />
+		<meta name="keywords" content="science without borders, alumni, exchange, swb" />
+		<meta name="author" content="Science without Borders PUCPR" />
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.3.0/css/font-awesome.css" />
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.3.0/css/font-awesome.min.css" />
+		<link href='http://fonts.googleapis.com/css?family=Raleway:200,400,800' rel='stylesheet' type='text/css'>
+		<script src="jquery-1.11.2.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    
+
+
+		<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body> 
+
+		<!-- Inserir no header, logo após a abertura do body -->
+		<div id="barra_institucional">
+		  <div class="cont_barra_inst">
+		      <div class="menu_institucional">
+		        <a href="http://www.pucpr.br/institucional/index.php" target="_blank">
+		          <div class="menu_inst_1" >
+		            A Universidade
+		          </div>
+		        </a>
+		        <a class="menu_inst_2" href="http://www.grupomarista.org.br/" target="_blank">
+		          <div class="menu_inst_2" >
+		            Grupo Marista
+		          </div>
+		        </a>
+		        <a class="menu_inst_3" href="http://www.pucpr.br" target="_blank">
+		          <div class="menu_inst_3" >
+		            Ir para o site da PUCPR
+		          </div>
+		        </a>
+		      </div>
+		      <a href="http://www.pucpr.br" target="_blank">
+		        <div class="logo_pucpr_topo">
+		        </div>
+		      </a>
+		  </div>
+		</div>
+
+		<div class="container demo-1">
+			<div class="content">
+
+				<nav>
+					<ul id="menu-nav">
+						<li><a href="#large-header">Home</a></li>
+                        <li><a href="#sobre-encontro">O encontro</a></li>
+						<li><a href="#programacao">Programação</a></li>
+						<li><a href="#participacao">Inscrição</a></li>
+						<li><a href="#contato">Contato</a></li>
+					</ul>
+				</nav>
+
+				<div class="info-data-evento">
+					<h1>June 10th 2015</h1>
+					<h4><i class="fa fa-map-marker"></i> Bloco Verde, PUCPR</h4>
+					<h4><i class="fa fa-clock-o"></i> 18h00</h4>
+				</div>
+
+				
+				<div id="large-header" class="large-header">
+					<canvas id="demo-canvas"></canvas>
+					<h1 class="main-title">
+						<img src="img/logo-teste-csf-encontro.png" class="logo-evento"/>
+					</h1>
+				</div>
+				
+				
+			</div> 
+
+
+			<!-- Related demos -->
+			<section class="section-size sobre-enc" id="sobre-encontro" data-sr="enter left, hustle 20px">
+				<h2 class="title-sobre-encontro">O encontro</h2>
+                
+                <p>Este evento traz a oportunidade de encontro de todos os estudantes bolsistas do Ciência sem Fronteiras da PUCPR que já retornaram para PUCPR, criando um espaço para compartilhar experiências, relembrar das coisas boas da viagem.</p>
+ 
+                <p>A PUCPR também quer saber dos bolsistas quais foram as melhores experiências que tiveram na Universidade Destino, bem como as experiências ruins.</p>
+ 
+                <p>Você tem alguma sugestão de coisas que viu lá fora e acha que seria ótimo e viável ter na PUCPR? Ou vivenciou algum problema que poderia ter sido evitado?</p>
+ 
+                <p>Venha para o nosso evento e compartilhe suas experiências! Sua opinião será extremamente importante, pois suas sugestões irão diretamente para a Reitoria da PUCPR, para o CNPq/CAPES e para o parceiro do país de destino. Juntos podemos fazer a diferença!</p>
+ 
+<p>E além disso, teremos a oportunidade de confraternização no momento do Welcome Coffee!</p>
+ 
+<p>Esperamos encontrá-los aqui!</p>
+                <br />
+                <h5>Declarações e Premiações</h5>
+                 
+                <p>Após o evento você poderá imprimir uma declaração de sua participação no SWB Experience.</p>
+                <p>Esta declaração poderá ser aproveitada nas horas complementares de seu curso, bem como no seu curricullum lattes, pois demonstrará que você está engajado no processo de construir um Brasil melhor, retribuindo o investimento que recebeu do Governo Federal.</p>
+
+                <p>As melhores ideias serão premiadas com uma Menção Honrosa:<br />
+                1) Melhor Ideia para a PUCPR;<br />
+                2) Melhor Ideia para a Gestão do Programa para CAPES/CNPq;<br />
+                3) Melhor Ideia para o parceiro do país de destino.</p>
+
+				
+
+			</section>
+
+			<section class="section-size schedule" id="programacao" data-sr="enter left, hustle 20px">
+				<h2 class="title-sobre-encontro">Programação</h2>
+
+				<table>
+				  <tr>
+				    <td><strong>18h00</strong></td>
+				    <td><img src="img/coffee.png" class="icon" /> Welcome Coffee.</td>
+				  </tr>
+				  <tr>
+				    <td><strong>18h45</strong></td>
+				    <td><img src="img/page_with_curl.png" class="icon" /> Apresentação das Autoridades.</td>
+				  </tr>
+				  <tr>
+				  	<td><strong>19h30</strong></td>
+				  	<td><img src="img/speech_balloon.png" class="icon" /> Palestra com o Prof. Fernando Luciano, professor PUCPR, palestrante TED: "Viajar para Liderar, isso faz sentido?" </td>
+				  </tr>
+                    
+                    <tr>
+				  	<td><strong>20h15</strong></td>
+				  	<td><img src="img/pencil.png" class="icon" /> Espaço Ideias e Sugestões.</td>
+				  </tr>
+                    
+                    <tr>
+				  	<td><strong>21h15</strong></td>
+				  	<td><img src="img/thumbsup.png" class="icon" /> Novas oportunidades para estudantes Ciência sem Fronteiras.</td>
+				  </tr>
+				</table>
+
+			</section>
+
+			<section class="section-size2 sobre-enc form-inc" id="participacao" data-sr="enter left, hustle 20px">
+				<h2 class="title-sobre-encontro">Inscrição</h2>
+
+
+                
+				<p>Preencha seu CPF para iniciar seu cadastro:</p>
+				<br />
+                <label for="cpf">CPF</label><br />
+                <input class="input-inscricao"></input>
+                
+				<button class="botao-enviar">Cadastrar</button>
+				
+                <br /><br /><br />
+                
+            <!-- SEGUNDA PARTE DO CADASTRO-->
+            
+<!--
+                <div class="colunas">
+                    
+                    <div class="coluna-1">
+                        <label for="cpf" class="label-esp"><b>Nome</b></label><br />
+                        <input class="input-inscricao preencher"></input><br />
+                        <br />
+                    
+                        <label for="cpf" class="label-esp"><b>Email</b></label><br />
+                        <input class="input-inscricao preencher"></input><br />
+                        <br />
+                        <label for="cpf"><b>Curso da PUCPR</b></label><br />
+                        <input class="input-inscricao preencher"></input><br />
+                        <br />
+                        <label for="cpf"><b>Universidade de destino</b></label><br />
+                        <input class="input-inscricao preencher"></input><br />
+                        <br />
+                        <label for="cpf"><b>País de Destino</b></label><br />
+                        <input class="input-inscricao preencher"></input><br />
+                        <br />
+                        <label for="cpf"><b>Início e Término da vigência da bolsa</b></label><br />
+                        Início 
+                        <select class="selecionar">
+                          <option value="janeiro">janeiro</option>
+                          <option value="fevereiro">fevereiro</option>
+                          <option value="marco">março</option>
+                          <option value="abril">abril</option>
+                            <option value="maio">maio</option>
+                          <option value="junho">junho</option>
+                          <option value="julho">julho</option>
+                          <option value="agosto">agosto</option>
+                            <option value="setembro">setembro</option>
+                          <option value="outubro">outubro</option>
+                          <option value="novembro">novembro</option>
+                          <option value="dezembro">dezembro</option>
+                        </select>
+    
+                        <select>
+                          <option value="2009">2009</option>
+                          <option value="2010">2010</option>
+                          <option value="2011">2011</option>
+                          <option value="2012">2012</option>
+                          <option value="2013">2013</option>
+                          <option value="2014">2014</option>
+                          <option value="2015">2015</option>
+                        </select>
+                        <br />
+        
+                        Término 
+                        <select>
+                          <option value="janeiro">janeiro</option>
+                          <option value="fevereiro">fevereiro</option>
+                          <option value="marco">março</option>
+                          <option value="abril">abril</option>
+                            <option value="maio">maio</option>
+                          <option value="junho">junho</option>
+                          <option value="julho">julho</option>
+                          <option value="agosto">agosto</option>
+                            <option value="setembro">setembro</option>
+                          <option value="outubro">outubro</option>
+                          <option value="novembro">novembro</option>
+                          <option value="dezembro">dezembro</option>
+                        </select>
+    
+                        <select>
+                          <option value="2009">2009</option>
+                          <option value="2010">2010</option>
+                          <option value="2011">2011</option>
+                          <option value="2012">2012</option>
+                          <option value="2013">2013</option>
+                          <option value="2014">2014</option>
+                          <option value="2015">2015</option>
+                        </select><br />
+    
+    
+                        <br />
+                        <label for="cpf"><b>Mês e ano de retorno à PUCPR</b></label><br />
+                        <select>
+                          <option value="janeiro">janeiro</option>
+                          <option value="fevereiro">fevereiro</option>
+                          <option value="marco">março</option>
+                          <option value="abril">abril</option>
+                            <option value="maio">maio</option>
+                          <option value="junho">junho</option>
+                          <option value="julho">julho</option>
+                          <option value="agosto">agosto</option>
+                            <option value="setembro">setembro</option>
+                          <option value="outubro">outubro</option>
+                          <option value="novembro">novembro</option>
+                          <option value="dezembro">dezembro</option>
+                        </select>
+    
+                        <select>
+                          <option value="2009">2009</option>
+                          <option value="2010">2010</option>
+                          <option value="2011">2011</option>
+                          <option value="2012">2012</option>
+                          <option value="2013">2013</option>
+                          <option value="2014">2014</option>
+                          <option value="2015">2015</option>
+                        </select><br />
+        
+                    </div>
+                    <div class="coluna-2">
+                        <label for="cpf"><b>Você fez estágio durante o intercâmbio?</b></label><br />
+                        <input type="radio" name="sim" value="sim" >Sim. Onde? <input class="input-inscricao"></input><br />
+                        <input type="radio" name="sim" value="sim" >Não<br />
+                        <br />
+                        <label for="cpf"><b>Você fez pesquisa durante o intercâmbio?</b></label><br />
+                        <input type="radio" name="sim" value="sim" >Sim. Onde? <input class="input-inscricao"></input><br />
+                        <input type="radio" name="sim" value="sim" >Não<br />
+                        <br />
+
+                        <label for="cpf"><b>Você está trabalhando?</b></label><br />
+                        <input type="radio" name="sim" value="sim" >Sim. Onde? <input class="input-inscricao"></input><br />
+                        <input type="radio" name="sim" value="sim" >Não<br />
+                        <br />
+                        <label for="cpf"><b>Vocês está em algum programa de mestrado?</b></label><br />
+                        <input type="radio" name="sim" value="sim" >Sim. Onde? <input class="input-inscricao"></input><br />
+<input type="radio" name="sim" value="sim" >Em processo de seleção. Onde? <input class="input-inscricao"></input><br />
+                        <input type="radio" name="sim" value="sim" >Não<br />
+                        <br />
+                        <label for="cpf"><b>Vocês está em algum programa de doutorado?</b></label><br />
+                        <input type="radio" name="sim" value="sim" >Sim. Onde? <input class="input-inscricao"></input><br />
+<input type="radio" name="sim" value="sim" >Em processo de seleção. Onde? <input class="input-inscricao"></input><br />
+                        <input type="radio" name="sim" value="sim" >Não<br />
+                        
+                        <br />
+                        <button class="botao-enviar">Finalizar minha inscrição</button>
+                    </div>
+                
+                </div>
+-->
+			
+
+			</section>
+
+			<section class="section-size" id="contato" data-sr="enter left, hustle 20px">
+				<h2 class="title-sobre-encontro">Contato</h2>
+
+				<p style="color: #fff;">Se você tiver alguma dúvida, entre em contato conosco:</p>
+				<br />
+
+				<ul class="lista-contato">
+					<li  style="color: #fff;">pibicpr@pucpr.br</li>
+					<li  style="color: #fff;">+55 41 3271-2112</li>
+					<li><p  style="color: #fff;">Rua Imaculada Conceição, 1155<br />
+						Bairro Prado Velho<br />
+						CEP: 80215-901</p></li>
+				</ul>
+
+				
+
+			</section>
+
+		</div><!-- /container -->
+		
+		<!-- Arquivos referentes a  animação do canvas. -->
+		<script src="js/TweenLite.min.js"></script>
+		<script src="js/EasePack.min.js"></script>
+		<script src="js/rAF.js"></script>
+		<script src="js/demo-1.js"></script>
+
+        <script>
+            $(function() {
+              $('a[href*=#]:not([href=#])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                  var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html,body').animate({
+                      scrollTop: target.offset().top
+                    }, 500);
+                    return false;
+                  }
+                }
+              });
+            });
+        </script>
+
+        
+
+
+<!--
+	    <script>
+        
+              $(window).scroll(function() {
+               var height = $(window).scrollTop();
+
+               if(height  > 7) {
+                   $($("header")).removeClass("abc")
+                   $($("header")).addClass("abc2")
+               }
+               else
+               {
+                   $($("header")).removeClass("abc2")
+                   $($("header")).addClass("abc")
+               }
+            });
+
+          </script>
+-->
+
+        <script src='js/scrollReveal.min.js'></script>
+	    <script>
+
+	      window.sr = new scrollReveal();
+	    </script>
+
+	</body>
+</html>
+
